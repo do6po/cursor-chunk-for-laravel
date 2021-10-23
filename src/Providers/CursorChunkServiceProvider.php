146cfc0604d648e8947f0be05b1fd1f7e78ffe36
Бойко Сергей <boiko.s.wezom@gmail.com>
2021-10-23 15:10:37 +0300

    Added method for simple builder;

--- src/Providers/CursorChunkServiceProvider.php
+++ src/Providers/CursorChunkServiceProvider.php
@@ -4,6 +4,7 @@ namespace Do6po\LaravelCursorChunk\Providers;
 
 use Closure;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class CursorChunkServiceProvider extends ServiceProvider
@@ -24,6 +25,27 @@ class CursorChunkServiceProvider extends ServiceProvider
                         yield $chunk;
 
                         $chunk = $this->getModel()->newCollection();
+                    }
+                }
+
+                yield $chunk;
+            }
+        );
+
+        \Illuminate\Database\Query\Builder::macro(
+            'cursorChunk',
+            function (int $limit = 500, Closure $action = null) {
+                /** @var Builder $this */
+                $chunk = Collection::make();
+                foreach ($this->cursor() as $model) {
+                    $action
+                        ? $action($chunk, $model)
+                        : $chunk->push($model);
+
+                    if ($chunk->count() >= $limit) {
+                        yield $chunk;
+
+                        $chunk = Collection::make();
                     }
                 }
 
